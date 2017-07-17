182120537d04c7d2a41edf24db77a2872e902b77
jimmie <jimmie@neisep.com>
2017-07-17 22:19:24 +0200

    Added method for listen on new message and download content to file on harddrive.

---
+++ emailpipe/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace emailpipe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<ListViewItem> _emailList = new ObservableCollection<ListViewItem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(10, GridUnitType.Star) });
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(10, GridUnitType.Star) });
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100, GridUnitType.Star) });
+
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(30, GridUnitType.Star) });
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(100, GridUnitType.Star) });
+
+            MainGrid.Background = Brushes.Pink;
+
+            GridView gridView = new GridView();
+            gridView.Columns.Add(new GridViewColumn { Header = "Subject", Width = 250, DisplayMemberBinding = new Binding("Subject") });
+            gridView.Columns.Add(new GridViewColumn { Header = "Date", Width = 80, DisplayMemberBinding = new Binding("Date") });
+
+            EmailListView.SetValue(Grid.ColumnProperty, 1);
+            EmailListView.SetValue(Grid.RowProperty, 2);
+            EmailListView.View = gridView;
+            EmailListView.ItemsSource = _emailList;
+            StartListen();
+            EmailListView.Opacity = 0.7;
+
+            Canvas test = new Canvas();
+            test.Background = Brushes.Orange;
+            test.Children.Add(new TextBlock { Text = "Simple email pipe for helpdesks", FontWeight = FontWeights.UltraBlack});
+            
+            test.SetValue(Grid.ColumnSpanProperty, 2);
+
+            MainGrid.Children.Add(test);
+
+            AddControllerButtons();
+        }
+
+        private void AddControllerButtons()
+        {
+            Grid grid = new Grid();
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50, GridUnitType.Star) });
+
+            grid.SetValue(Grid.RowProperty, 2);
+
+
+            Button refreshButton = new Button();
+            refreshButton.Content = "Refresh list";
+            refreshButton.SetValue(Grid.RowProperty, 1);
+
+            Button resendToIntegrationButton = new Button();
+            resendToIntegrationButton.Content = new TextBlock { Text = "Upload to integration", TextWrapping = TextWrapping.Wrap };
+            resendToIntegrationButton.SetValue(Grid.RowSpanProperty, 2);
+            resendToIntegrationButton.SetValue(Grid.RowProperty, 2);
+
+            Button clearButton = new Button();
+            clearButton.Content = "Clear list";
+            clearButton.SetValue(Grid.RowProperty, 9);
+
+            Button settingsButton = new Button();
+            settingsButton.Content = "Settings";
+            settingsButton.SetValue(Grid.RowProperty, 8);
+
+            grid.Children.Add(resendToIntegrationButton);
+            grid.Children.Add(refreshButton);
+            grid.Children.Add(clearButton);
+            grid.Children.Add(settingsButton);
+
+            MainGrid.Children.Add(grid);
+        }
+
+        private void StartListen()
+        {
+            imap imap = new imap("localhost", 143, "test@localhost.com", "password", _emailList);
+            imap.Listen();
+        }
+
+        
+         
+    }
+}
---
+++ emailpipe/imap.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using S22.Imap;
+using System.Net.Mail;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+
+namespace emailpipe
+{
+    public class imap
+    {
+        private ObservableCollection<ListViewItem> _emailListViewItem;
+        private string _ip;
+        private int _port;
+        private string _username;
+        private string _password;
+        public imap(string ip, int port, string username, string password, ObservableCollection<ListViewItem> emailListViewItem)
+        {
+            _ip = ip;
+            _port = port;
+            _username = username;
+            _password = password;
+            _emailListViewItem = emailListViewItem;
+        }
+
+        public void Listen()
+        {
+            Task test = new Task(() =>
+            {
+                try
+                {
+                    using (ImapClient imapClient = new ImapClient(_ip, _port, _username, _password))
+                    {
+                        if (!imapClient.Supports("IDLE"))
+                        {
+                            MessageBox.Show("Mail server does not support IDLE command");
+                            return;
+                        }
+
+                        imapClient.NewMessage += new EventHandler<IdleMessageEventArgs>(ImapClient_NewMessage);
+
+                        while (true)
+                        {
+                            System.Threading.Thread.Sleep(6000);
+                        }
+                    }
+                }
+                catch (Exception crap)
+                {
+                    //TODO FIX SOME ERROR CODE OR SOMETHING HERE!
+                }
+
+            });
+
+            test.Start();
+        }
+
+        private void ImapClient_NewMessage(object sender, IdleMessageEventArgs e)
+        {
+            MailMessage eml = e.Client.GetMessage(e.MessageUID, FetchOptions.Normal);
+            var header = eml.Headers;
+
+            StringBuilder messageString = new StringBuilder();
+
+            //Puts together the header.
+            for (int i = 0; i < header.Count; i++)
+            {
+                messageString.Append("" + header.Keys[i] + ": " + header[i].ToString() + "");
+                messageString.AppendLine();
+            }
+
+            messageString.AppendLine();
+            messageString.Append(eml.Body);
+
+            var data = Encoding.UTF8.GetBytes(messageString.ToString());
+
+            var runningDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            runningDir = runningDir + "\\emails";
+
+            if (!System.IO.Directory.Exists(runningDir))
+                System.IO.Directory.CreateDirectory(runningDir);
+
+            string fileName = string.Format("{1}\\email_{0}.eml", e.MessageUID, runningDir);
+            File.WriteAllBytes(fileName, data);
+
+            Application.Current.Dispatcher.Invoke((Action)delegate
+            {
+                ListViewItem test = new ListViewItem();
+
+                string emailDate = DateTime.Now.ToString();
+                if (eml.Date() != null)
+                    emailDate = ((DateTime)eml.Date()).ToString("yyyy-MM-dd HH:mm:ss");
+
+                test.Content = new EmailListViewItem { Subject = eml.Subject, Date = emailDate };
+                _emailListViewItem.Add(test);
+            });
+
+        }
+    }
+
+    public class EmailListViewItem
+    {
+        public string Subject { get; set; }
+        public string Date { get; set; }
+    }
+}
